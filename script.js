c48a67268f06ea6f915fe86679bd202cacbb58dc
Filip <filip.asterbom@skola.taby.se>
2023-04-03 15:30:23 +0200

    Co-authored-by: Linus von von von van blomgren <Blomman76@users.noreply.github.com>

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ gameCanvas.height = SCREENHEIGHT / 2;
 gameCanvas.width = SCREENWIDTH / 2;
 // -------------------------------------
 // Player variables
+
+let cat = new Image();
+let mus = new Image();
+
+cat.onload=function(){
+}
+cat.src="Bild/gullig_cat.png"
+
+mus.onload=function(){
+}
+mus.src="Bild/mus.png"
+
+
 let playerX = 200;
 let playerY = 200;
 let playerWidth = 10;
@@ -15,12 +28,24 @@ let playerHeight = 10;
 let dx = 2;
 let dy = 2;
 
+let ax = 2;
+let ay = 2;
+
+
 let directions = {
   left: false,
   right: false,
   up: false,
   down: false,
 };
+
+let directions1 = {
+  left: false,
+  right: false,
+  up: false,
+  down: false,
+};
+
 // -------------------------------------
 // ------------ Player movement ------------
 document.addEventListener("keydown", (e) => {
@@ -37,8 +62,21 @@ document.addEventListener("keydown", (e) => {
     case "ArrowDown":
       directions.down = true;
       break;
-    default:
+    case "a":
+      directions1.left = true;
       break;
+    case "d":
+      directions1.right = true;
+      break;
+    case "w":
+      directions1.up = true; 
+      break;   
+    case "s":
+      directions1.down = true;
+      break;
+    
+      default:
+    break;
   }
 });
 
@@ -56,39 +94,66 @@ document.addEventListener("keyup", (e) => {
     case "ArrowDown":
       directions.down = false;
       break;
-    default:
+    case "a":
+      directions1.left = false;
+      break;
+    case "d":
+      directions1.right = false;
+      break;
+    case "w":
+      directions1.up = false;  
+      break;
+    case "s":
+      directions1.down = false;
+      break;  
+    
+      default:
       break;
   }
-});
-let cat = document.getElementById("cat")
-let mus = document.getElementById("mus")
-// -------------------------------------
+ 
+}); 
 // ------------ Animation ------------
+// -------------------------------------
+// let cat = document.getElementById("cat")
+// let mus = document.getElementById("mus")
 function animate() {
   requestAnimationFrame(animate); // Run gameloop recursively
   c.clearRect(0, 0, innerWidth, innerHeight); // Clear screen
-    console.log("hej")
-  c.fillRect(playerX, playerY, playerWidth, playerHeight); // Draw player
+  console.log(directions1)
 
- c.drawImage(cat, dx, dy, 40, 40)
+  // c.fillRect(playerX, playerY, playerWidth, playerHeight); // Draw player
+
+  c.drawImage(cat, playerX, playerY, 40, 40)
+  c.drawImage(mus, playerX, playerY, 40, 40)
  
 
   if (directions.right && playerX < SCREENWIDTH / 2 - playerWidth) {
-    playerX += dx;
+    playerX += dx; 
   }
-
-  if (directions.left && playerX > 0) {
+  else if (directions.left && playerX > 0) {
     playerX -= dx;
   }
-
-  if (directions.up && playerY > 0) {
-    playerY -= dy;
+  else if (directions.up && playerY > 0) {
+    playerY -= dy; 
   }
-
-  if (directions.down && playerY < SCREENHEIGHT / 2 - playerHeight) {
+  else if (directions.down && playerY < SCREENHEIGHT / 2 - playerHeight) {
     playerY += dy;
   }
+  else if (directions.right && playerX < SCREENWIDTH / 2 - playerWidth) {
+    playerX += ax; 
+  }
+  else if (directions.left && playerX > 0) {
+    playerX -= ax;
+  }
+  else if (directions.up && playerY > 0) {
+    playerY -= ay; 
+  }
+  else if (directions.down && playerY < SCREENHEIGHT / 2 - playerHeight) {
+    playerY += ay;
+  }
+
 }
 // -------------------------------------
 // ------------ Start game ------------
+
 animate();
